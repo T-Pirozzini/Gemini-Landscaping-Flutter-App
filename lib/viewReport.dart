7d8137fbc0d27ede89c0cff3f21d00925d924f60
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2023-02-02 17:35:28 -0800

    viewReport displays date, site, names and times

--- lib/viewReport.dart
+++ lib/viewReport.dart
@@ -51,278 +51,76 @@ class _ViewReportState extends State<ViewReport> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              // date picker
-              Container(
-                decoration: BoxDecoration(border: Border.all()),
-                child: Text(widget.docid.get('date')),
+              Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                // crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(border: Border.all()),
+                    child: Text(widget.docid.get('date')),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(widget.docid.get("siteName")),
+                ],
               ),
-              const SizedBox(
-                height: 10,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    widget.docid.get("name1"),
+                  ),
+                  Text(
+                    "On: " + widget.docid.get("timeOn1"),
+                  ),
+                  Text(
+                    "Off: " + widget.docid.get("timeOff1"),
+                  ),
+                ],
               ),
-              // site list drop down
-              Text(widget.docid.get("siteName")),
-              const SizedBox(
-                height: 10,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    widget.docid.get("name2"),
+                  ),
+                  Text(
+                    "On: " + widget.docid.get("timeOn2"),
+                  ),
+                  Text(
+                    "Off: " + widget.docid.get("timeOff2"),
+                  ),
+                ],
               ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //   children: [
-              //     Expanded(
-              //       // decoration: BoxDecoration(border: Border.all()),
-              //       child: TextField(
-              //         controller: name1,
-              //         maxLines: null,
-              //         keyboardType: TextInputType.text,
-              //         decoration: const InputDecoration(
-              //           hintText: 'Driver',
-              //         ),
-              //       ),
-              //     ),
-              //     Column(
-              //       children: [
-              //         const Text("On"),
-              //         FloatingActionButton.extended(
-              //           heroTag: "btn1On",
-              //           icon: const Icon(Icons.access_time_outlined),
-              //           label: Text(
-              //               '${timeOn1!.hour.toString()}:${timeOn1!.minute.toString()}'),
-              //           backgroundColor: const Color.fromARGB(255, 31, 182, 77),
-              //           onPressed: () async {
-              //             TimeOfDay? newTimeOn1 = await showTimePicker(
-              //               context: context,
-              //               initialTime: timeOn1!,
-              //             );
-              //             if (newTimeOn1 != null) {
-              //               setState(() {
-              //                 timeOn1 = newTimeOn1;
-              //               });
-              //             }
-              //           },
-              //         ),
-              //       ],
-              //     ),
-              //     const SizedBox(
-              //       width: 10,
-              //     ),
-              //     Column(
-              //       children: [
-              //         const Text("Off"),
-              //         FloatingActionButton.extended(
-              //           heroTag: "btn1Off",
-              //           icon: const Icon(Icons.access_time_outlined),
-              //           label: Text(
-              //               '${timeOff1!.hour.toString()}:${timeOff1!.minute.toString()}'),
-              //           backgroundColor: const Color.fromARGB(255, 31, 182, 77),
-              //           onPressed: () async {
-              //             TimeOfDay? newTimeOff1 = await showTimePicker(
-              //               context: context,
-              //               initialTime: timeOff1!,
-              //             );
-              //             if (newTimeOff1 != null) {
-              //               setState(() {
-              //                 timeOff1 = newTimeOff1;
-              //               });
-              //             }
-              //           },
-              //         ),
-              //       ],
-              //     ),
-              //   ],
-              // ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //   children: [
-              //     Expanded(
-              //       child: TextField(
-              //         controller: name2,
-              //         maxLines: null,
-              //         keyboardType: TextInputType.text,
-              //         decoration: const InputDecoration(
-              //           hintText: 'Name',
-              //         ),
-              //       ),
-              //     ),
-              //     Column(
-              //       children: [
-              //         const SizedBox(height: 4),
-              //         FloatingActionButton.extended(
-              //           heroTag: "btn2On",
-              //           icon: const Icon(Icons.access_time_outlined),
-              //           label: Text(
-              //               '${timeOn2!.hour.toString()}:${timeOn2!.minute.toString()}'),
-              //           backgroundColor: const Color.fromARGB(255, 31, 182, 77),
-              //           onPressed: () async {
-              //             TimeOfDay? newTimeOn2 = await showTimePicker(
-              //               context: context,
-              //               initialTime: timeOn2!,
-              //             );
-              //             if (newTimeOn2 != null) {
-              //               setState(() {
-              //                 timeOn2 = newTimeOn2;
-              //               });
-              //             }
-              //           },
-              //         ),
-              //       ],
-              //     ),
-              //     const SizedBox(
-              //       width: 10,
-              //     ),
-              //     Column(
-              //       children: [
-              //         const SizedBox(height: 4),
-              //         FloatingActionButton.extended(
-              //           heroTag: "btn2Off",
-              //           icon: const Icon(Icons.access_time_outlined),
-              //           label: Text(
-              //               '${timeOff2!.hour.toString()}:${timeOff2!.minute.toString()}'),
-              //           backgroundColor: const Color.fromARGB(255, 31, 182, 77),
-              //           onPressed: () async {
-              //             TimeOfDay? newTimeOff2 = await showTimePicker(
-              //               context: context,
-              //               initialTime: timeOff2!,
-              //             );
-              //             if (newTimeOff2 != null) {
-              //               setState(() {
-              //                 timeOff2 = newTimeOff2;
-              //               });
-              //             }
-              //           },
-              //         ),
-              //       ],
-              //     ),
-              //   ],
-              // ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //   children: [
-              //     Expanded(
-              //       // decoration: BoxDecoration(border: Border.all()),
-              //       child: TextField(
-              //         controller: name3,
-              //         maxLines: null,
-              //         keyboardType: TextInputType.text,
-              //         decoration: const InputDecoration(
-              //           hintText: 'Name',
-              //         ),
-              //       ),
-              //     ),
-              //     Column(
-              //       children: [
-              //         const SizedBox(height: 4),
-              //         FloatingActionButton.extended(
-              //           heroTag: "btn3On",
-              //           icon: const Icon(Icons.access_time_outlined),
-              //           label: Text(
-              //               '${timeOn3!.hour.toString()}:${timeOn3!.minute.toString()}'),
-              //           backgroundColor: const Color.fromARGB(255, 31, 182, 77),
-              //           onPressed: () async {
-              //             TimeOfDay? newTimeOn3 = await showTimePicker(
-              //               context: context,
-              //               initialTime: timeOn3!,
-              //             );
-              //             if (newTimeOn3 != null) {
-              //               setState(() {
-              //                 timeOn3 = newTimeOn3;
-              //               });
-              //             }
-              //           },
-              //         ),
-              //       ],
-              //     ),
-              //     const SizedBox(
-              //       width: 10,
-              //     ),
-              //     Column(
-              //       children: [
-              //         const SizedBox(height: 4),
-              //         FloatingActionButton.extended(
-              //           heroTag: "btn3Off",
-              //           icon: const Icon(Icons.access_time_outlined),
-              //           label: Text(
-              //               '${timeOff3!.hour.toString()}:${timeOff3!.minute.toString()}'),
-              //           backgroundColor: const Color.fromARGB(255, 31, 182, 77),
-              //           onPressed: () async {
-              //             TimeOfDay? newTimeOff3 = await showTimePicker(
-              //               context: context,
-              //               initialTime: timeOff3!,
-              //             );
-              //             if (newTimeOff3 != null) {
-              //               setState(() {
-              //                 timeOff3 = newTimeOff3;
-              //               });
-              //             }
-              //           },
-              //         ),
-              //       ],
-              //     ),
-              //   ],
-              // ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              //   children: [
-              //     Expanded(
-              //       // decoration: BoxDecoration(border: Border.all()),
-              //       child: TextField(
-              //         controller: name4,
-              //         maxLines: null,
-              //         keyboardType: TextInputType.text,
-              //         decoration: const InputDecoration(
-              //           hintText: 'Name',
-              //         ),
-              //       ),
-              //     ),
-              //     Column(
-              //       children: [
-              //         const SizedBox(height: 4),
-              //         FloatingActionButton.extended(
-              //           heroTag: "btn4On",
-              //           icon: const Icon(Icons.access_time_outlined),
-              //           label: Text(
-              //               '${timeOn4!.hour.toString()}:${timeOn4!.minute.toString()}'),
-              //           backgroundColor: const Color.fromARGB(255, 31, 182, 77),
-              //           onPressed: () async {
-              //             TimeOfDay? newTimeOn4 = await showTimePicker(
-              //               context: context,
-              //               initialTime: timeOn4!,
-              //             );
-              //             if (newTimeOn4 != null) {
-              //               setState(() {
-              //                 timeOn4 = newTimeOn4;
-              //               });
-              //             }
-              //           },
-              //         ),
-              //       ],
-              //     ),
-              //     const SizedBox(
-              //       width: 10,
-              //     ),
-              //     Column(
-              //       children: [
-              //         const SizedBox(height: 4),
-              //         FloatingActionButton.extended(
-              //           heroTag: "btn4Off",
-              //           icon: const Icon(Icons.access_time_outlined),
-              //           label: Text(
-              //               '${timeOff4!.hour.toString()}:${timeOff4!.minute.toString()}'),
-              //           backgroundColor: const Color.fromARGB(255, 31, 182, 77),
-              //           onPressed: () async {
-              //             TimeOfDay? newTimeOff4 = await showTimePicker(
-              //               context: context,
-              //               initialTime: timeOff4!,
-              //             );
-              //             if (newTimeOff4 != null) {
-              //               setState(() {
-              //                 timeOff4 = newTimeOff4;
-              //               });
-              //             }
-              //           },
-              //         ),
-              //       ],
-              //     ),
-              //   ],
-              // ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    widget.docid.get("name3"),
+                  ),
+                  Text(
+                    "On: " + widget.docid.get("timeOn3"),
+                  ),
+                  Text(
+                    "Off: " + widget.docid.get("timeOff3"),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    widget.docid.get("name4"),
+                  ),
+                  Text(
+                    "On: " + widget.docid.get("timeOn4"),
+                  ),
+                  Text(
+                    "Off: " + widget.docid.get("timeOff4"),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
