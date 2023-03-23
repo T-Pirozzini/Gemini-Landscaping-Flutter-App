a093fd31900d490f6d2ec991eb42bf13ce9ce63d
T-Pirozzini <tpirozzini@gmail.com>
2023-03-23 13:20:17 -0700

    add floating action bubble and implement

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,8 +5,8 @@ import 'package:gemini_landscaping_app/pages/profile_page.dart';
 import 'package:gemini_landscaping_app/pages/reports_page.dart';
 import '../addReport.dart';
 import '../auth.dart';
-import 'auth_page.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:floating_action_bubble/floating_action_bubble.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -15,15 +15,30 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-// // get current user
-// final currentUser = FirebaseAuth.instance.currentUser!;
-
 final User? user = Auth().currentUser;
 
 bool _sortBySiteName = false;
 bool _sortByDate = false;
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  // floating action bubble
+  late Animation<double> _animation;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 260),
+    );
+
+    final curvedAnimation =
+        CurvedAnimation(curve: Curves.easeInOut, parent: _animationController);
+    _animation = Tween<double>(begin: 0, end: 1).animate(curvedAnimation);
+
+    super.initState();
+  }
+
   // bottom navigation bar
   int currentIndex = 0;
   final pages = [
@@ -36,16 +51,6 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade600,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromARGB(255, 31, 182, 77),
-        onPressed: () {
-          Navigator.pushReplacement(
-              context, MaterialPageRoute(builder: (_) => const AddReport()));
-        },
-        child: const Icon(
-          Icons.note_add_outlined,
-        ),
-      ),
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 31, 182, 77),
         title: const Text('SITE REPORTS 2023'),
@@ -69,6 +74,60 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionBubble(
+        items: <Bubble>[
+          // Floating action menu item
+          Bubble(
+            title: "Site Report",
+            iconColor: Colors.white,
+            bubbleColor: Color.fromARGB(255, 31, 182, 77),
+            icon: Icons.add,
+            titleStyle: TextStyle(fontSize: 16, color: Colors.white),
+            onPress: () {
+              _animationController.reverse();
+            },
+          ),
+          Bubble(
+            title: "Extras Report",
+            iconColor: Colors.white,
+            bubbleColor: Color.fromARGB(255, 31, 182, 77),
+            icon: Icons.add,
+            titleStyle: TextStyle(fontSize: 16, color: Colors.white),
+            onPress: () {
+              _animationController.reverse();
+            },
+          ),
+          Bubble(
+            title: "Pictures",
+            iconColor: Colors.white,
+            bubbleColor: Color.fromARGB(255, 31, 182, 77),
+            icon: Icons.add_a_photo_outlined,
+            titleStyle: TextStyle(fontSize: 16, color: Colors.white),
+            onPress: () {
+              _animationController.reverse();
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (_) => const AddReport()));
+            },
+          ),
+        ],
+        iconColor: Colors.white,
+        backGroundColor: const Color.fromARGB(255, 31, 182, 77),
+        animation: _animation,
+        // On pressed change animation state
+        onPress: () => _animationController.isCompleted
+            ? _animationController.reverse()
+            : _animationController.forward(),
+        // Flaoting Action button Icon
+        iconData: Icons.note_add_outlined,
+        // onPress: () {
+        //   Navigator.pushReplacement(
+        //       context, MaterialPageRoute(builder: (_) => const AddReport()));
+        // },
+        // child: const Icon(
+        //   Icons.note_add_outlined,
+        // ),
+      ),
       body: pages[currentIndex],
       bottomNavigationBar: CurvedNavigationBar(
         height: 60,
@@ -81,17 +140,17 @@ class _HomeState extends State<Home> {
           Icon(
             Icons.folder_copy_outlined,
             color: Colors.white,
-            size: 40,
+            size: 30,
           ),
           Icon(
             Icons.message_outlined,
             color: Colors.white,
-            size: 40,
+            size: 30,
           ),
           Icon(
             Icons.account_circle_outlined,
             color: Colors.white,
-            size: 40,
+            size: 30,
           ),
         ],
       ),
