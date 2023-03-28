646aef811d3ce9e429fca9afaf7d3abf6368cae3
T-Pirozzini <tpirozzini@gmail.com>
2023-03-27 18:44:23 -0700

    setup for chat feature implementation

--- lib/pages/announcement_page.dart
+++ lib/pages/announcement_page.dart
@@ -1,12 +1,94 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AnnouncementPage extends StatelessWidget {
+class AnnouncementPage extends StatefulWidget {
   const AnnouncementPage({super.key});
+
+  @override
+  _AnnouncementPageState createState() => _AnnouncementPageState();
+}
+
+class _AnnouncementPageState extends State<AnnouncementPage> {
+  String _messageText = '';
+
+  void _sendMessage() {
+    if (_messageText.isNotEmpty) {
+      FirebaseFirestore.instance.collection('messages').add({
+        'text': _messageText,
+        'senderId': FirebaseAuth.instance.currentUser!,
+        'receiverId': 'user2',
+        'timestamp': Timestamp.now(),
+      });
+      setState(() {
+        _messageText = '';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Announcement Page'),
+      appBar: AppBar(
+        title: Text('Announcements'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('messages')
+                  .orderBy('timestamp', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                final messages = snapshot.data!.docs;
+                return ListView.builder(
+                  reverse: true,
+                  itemCount: messages.length,
+                  itemBuilder: (context, index) {
+                    final message = messages[index].data();
+                    return ListTile(
+                        // title: Text(message?['text']?.toString() ?? ''),
+                        // subtitle: Text(message['senderId']?.toString() ?? ''),
+                        );
+                  },
+                );
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(hintText: 'Type a message'),
+                    onChanged: (value) {
+                      setState(() {
+                        _messageText = value;
+                      });
+                    },
+                    onSubmitted: (value) {
+                      _sendMessage();
+                    },
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    _sendMessage();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
