7816f96a634f902770ff2fa07acd2a2ba9ce794e
T-Pirozzini <tpirozzini@gmail.com>
2023-03-30 14:29:25 -0700

    final updates for version 2

--- lib/pages/viewReport.dart
+++ lib/pages/viewReport.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gemini_landscaping_app/pages/home_page.dart';
-import 'sitereport.dart';
+import '../sitereport.dart';
 
 // ignore: must_be_immutable
 class ViewReport extends StatefulWidget {
