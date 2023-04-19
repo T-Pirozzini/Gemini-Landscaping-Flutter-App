441e7e3237b16e11b91b7ebfcb712a08008a9f87
T-Pirozzini <tpirozzini@gmail.com>
2023-04-19 15:41:00 -0700

    setup bar chart - adjust base styles and pull siteName data to chart

--- lib/pages/chart_page.dart
+++ lib/pages/chart_page.dart
@@ -1,74 +1,81 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
 class Chart extends StatefulWidget {
-  const Chart({super.key});
+  const Chart({Key? key});
 
   @override
   State<Chart> createState() => _ChartState();
 }
 
 class _ChartState extends State<Chart> {
+  final Stream<QuerySnapshot> _siteStream =
+      FirebaseFirestore.instance.collectionGroup('SiteReports2023').snapshots();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 550,
-      child: SfCartesianChart(
-        title: ChartTitle(
-          text: "Car Sales",
-        ),
-        primaryXAxis: CategoryAxis(
-          title: AxisTitle(
-            text: "Car Names",
-          ),
-        ),
-        primaryYAxis: NumericAxis(
-          title: AxisTitle(
-            text: "Sales in Millions",
-          ),
-        ),
-        legend: Legend(
-          isVisible: true,
-        ),
-        series: <ChartSeries>[
-          ColumnSeries<SalesData, String>(
-            name: "Cars",
-            dataSource: getColumnData(),
-            xValueMapper: (SalesData sales, _) => sales.x,
-            yValueMapper: (SalesData sales, _) => sales.y,
-            dataLabelSettings: DataLabelSettings(
-              isVisible: true,
-              labelPosition: ChartDataLabelPosition.outside,
-            ),
-          )
-        ],
+      padding: EdgeInsets.fromLTRB(0, 8, 8, 40),
+      color: Colors.grey.shade200,
+      child: StreamBuilder<QuerySnapshot>(
+        stream: _siteStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            final data = getColumnData(snapshot.data!);
+            return SfCartesianChart(
+              title: ChartTitle(
+                text: "April 2023",
+              ),
+              primaryXAxis: CategoryAxis(
+                title: AxisTitle(
+                  text: "Sites",
+                ),
+                labelRotation: 90,
+              ),
+              primaryYAxis: NumericAxis(
+                title: AxisTitle(
+                  text: "Time (hours)",
+                ),
+              ),
+              legend: Legend(
+                isVisible: true,
+              ),
+              series: <ChartSeries>[
+                ColumnSeries<SiteData, String>(
+                  name: "Site Time",
+                  dataSource: data,
+                  xValueMapper: (SiteData data, _) => data.x,
+                  yValueMapper: (SiteData data, _) => data.y,
+                  dataLabelSettings: DataLabelSettings(
+                    // isVisible: true,
+                    labelPosition: ChartDataLabelPosition.outside,
+                  ),
+                ),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
       ),
     );
   }
 }
 
-class SalesData {
+class SiteData {
   String x;
   double y;
 
-  SalesData(this.x, this.y);
+  SiteData(this.x, this.y);
 }
 
-dynamic getColumnData() {
-  List<SalesData> ColumnData = <SalesData>[
-    SalesData("BMW", 20),
-    SalesData("Subaru", 30),
-    SalesData("Honda", 10),
-    SalesData("Tesla", 60),
-    SalesData("Toyota", 20),
-    SalesData("Mazda", 50),
-    SalesData("Hyundai", 10),
-    SalesData("Mitsubishi", 60),
-    SalesData("Ford", 20),
-    SalesData("Chevrolet", 30),
-    SalesData("GMC", 10),
-    SalesData("Volvo", 60),
-  ];
+List<SiteData> getColumnData(QuerySnapshot snapshot) {
+  final List<SiteData> ColumnData = <SiteData>[];
+  for (var doc in snapshot.docs) {
+    ColumnData.add(SiteData(doc['info']['siteName'], 20));
+  }
   return ColumnData;
 }
