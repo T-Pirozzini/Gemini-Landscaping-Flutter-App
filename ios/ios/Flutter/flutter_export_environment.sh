90f64671b19b385ffd742f74866197df1c0697a0
Travis Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2023-08-08 13:37:16 -0700

    sync continued

---
+++ ios/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/tpiro/documents/development/flutterInstall/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tpiro/Documents/GitHub/Gemini-Landscaping-Flutter-App/ios"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/tpiro/Documents/GitHub/Gemini-Landscaping-Flutter-App/ios/.dart_tool/package_config.json"
