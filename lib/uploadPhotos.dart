94ed859d7ef78ed42c02f64fcb1db821d128e83c
T-Pirozzini <tpirozzini@gmail.com>
2023-09-19 17:46:53 -0700

    implement firebase storage - upload phot page in progress

--- lib/uploadPhotos.dart
+++ lib/uploadPhotos.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:gemini_landscaping_app/pages/home_page.dart';
 import 'package:lottie/lottie.dart';
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadPhotos extends StatefulWidget {
   const UploadPhotos({super.key});
@@ -10,6 +13,40 @@ class UploadPhotos extends StatefulWidget {
 }
 
 class _UploadPhotosState extends State<UploadPhotos> {
+  File? _image;
+  final picker = ImagePicker();
+
+  Future<void> _uploadImage() async {
+    try {
+      final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+      if (pickedFile != null) {
+        final imageFile = File(pickedFile.path);
+
+        final Reference storageReference = FirebaseStorage.instance
+            .ref()
+            .child('images/${DateTime.now()}.jpg');
+
+        final UploadTask uploadTask = storageReference.putFile(imageFile);
+
+        await uploadTask.whenComplete(() {
+          // Handle the image upload completion.
+          // You can get the download URL here:
+          storageReference.getDownloadURL().then((imageUrl) {
+            // Save the imageUrl to Firestore or use it as needed.
+            print('Image URL: $imageUrl');
+          });
+        });
+
+        setState(() {
+          _image = imageFile;
+        });
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +79,16 @@ class _UploadPhotosState extends State<UploadPhotos> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Center(
+            child: _image != null
+                ? Image.file(_image!)
+                : Text('No image selected'),
+          ),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: _uploadImage,
+            child: Text('Upload Image'),
+          ),
           Center(
             child: Lottie.network(
               'https://assets8.lottiefiles.com/packages/lf20_0zv8teye.json',
