95392ef70d35a6bd8a486d37245cd443670ca74d
T-Pirozzini <tpirozzini@gmail.com>
2023-09-20 17:21:37 -0700

    upload photos, display in a grid view and allow download and enlarge of photos

--- lib/uploadPhotos.dart
+++ lib/uploadPhotos.dart
@@ -1,117 +1,14 @@
-// import 'package:flutter/material.dart';
-// import 'package:gemini_landscaping_app/pages/home_page.dart';
-// import 'package:lottie/lottie.dart';
-// import 'dart:io';
-// import 'package:firebase_storage/firebase_storage.dart';
-// import 'package:image_picker/image_picker.dart';
-
-// class UploadPhotos extends StatefulWidget {
-//   const UploadPhotos({super.key});
-
-//   @override
-//   State<UploadPhotos> createState() => _UploadPhotosState();
-// }
-
-// class _UploadPhotosState extends State<UploadPhotos> {
-//   File? _image;
-//   final picker = ImagePicker();
-
-//   Future<void> _uploadImage() async {
-//     try {
-//       final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-
-//       if (pickedFile != null) {
-//         final imageFile = File(pickedFile.path);
-
-//         final Reference storageReference = FirebaseStorage.instance
-//             .ref()
-//             .child('images/${DateTime.now()}.jpg');
-
-//         final UploadTask uploadTask = storageReference.putFile(imageFile);
-
-//         await uploadTask.whenComplete(() {
-//           // Handle the image upload completion.
-//           // You can get the download URL here:
-//           storageReference.getDownloadURL().then((imageUrl) {
-//             // Save the imageUrl to Firestore or use it as needed.
-//             print('Image URL: $imageUrl');
-//           });
-//         });
-
-//         setState(() {
-//           _image = imageFile;
-//         });
-//       }
-//     } catch (e) {
-//       print(e.toString());
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: const Color.fromARGB(255, 31, 182, 77),
-//         leading: MaterialButton(
-//           onPressed: () {
-//             Navigator.pushReplacement(
-//                 context, MaterialPageRoute(builder: (_) => Home()));
-//           },
-//           child: Row(
-//             children: const [
-//               Icon(Icons.arrow_circle_left_outlined,
-//                   color: Colors.white, size: 18),
-//               Text(
-//                 " Back",
-//                 style: TextStyle(
-//                   fontSize: 15,
-//                   color: Color.fromARGB(255, 251, 251, 251),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//         leadingWidth: 100,
-//         title: Image.asset("assets/gemini-icon-transparent.png",
-//             color: Colors.white, fit: BoxFit.contain, height: 50),
-//         centerTitle: true,
-//       ),
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Center(
-//             child: _image != null
-//                 ? Image.file(_image!)
-//                 : Text('No image selected'),
-//           ),
-//           SizedBox(height: 10),
-//           ElevatedButton(
-//             onPressed: _uploadImage,
-//             child: Text('Upload Image'),
-//           ),
-//           Center(
-//             child: Lottie.network(
-//               'https://assets8.lottiefiles.com/packages/lf20_0zv8teye.json',
-//               height: 200,
-//             ),
-//           ),
-//           SizedBox(height: 10),
-//           Text(
-//             'Coming Soon...',
-//             style: TextStyle(fontSize: 24),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-import 'package:flutter/material.dart';
+import 'dart:typed_data';
 import 'package:gemini_landscaping_app/pages/home_page.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:lottie/lottie.dart';
 import 'dart:io';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 class UploadPhotos extends StatefulWidget {
   const UploadPhotos({super.key});
@@ -121,25 +18,38 @@ class UploadPhotos extends StatefulWidget {
 }
 
 class _UploadPhotosState extends State<UploadPhotos> {
+  List<String> imageUrls = []; // List to store image URLs
   File? _image;
   final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchImagesFromFirebaseStorage();
+  }
 
   Future<void> _uploadImage() async {
     try {
       final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
       if (pickedFile != null) {
-        final imageFile = File(pickedFile.path);
+        final File imageFile = File(pickedFile.path);
+
+        // Compress the image before uploading
+        final Uint8List compressedImageData =
+            await FlutterImageCompress.compressWithFile(
+          imageFile.path,
+          quality: 70, // Adjust the quality as needed (0 - 100)
+        ) as Uint8List; // Explicitly cast it to Uint8List
 
         final Reference storageReference = FirebaseStorage.instance
             .ref()
             .child('images/${DateTime.now()}.jpg');
 
-        final UploadTask uploadTask = storageReference.putFile(imageFile);
+        final UploadTask uploadTask =
+            storageReference.putData(compressedImageData);
 
         await uploadTask.whenComplete(() {
-          // Handle the image upload completion.
-          // You can get the download URL here:
           storageReference.getDownloadURL().then((imageUrl) {
             // Save the imageUrl to Firestore or use it as needed.
             print('Image URL: $imageUrl');
@@ -147,7 +57,7 @@ class _UploadPhotosState extends State<UploadPhotos> {
         });
 
         setState(() {
-          _image = imageFile;
+          _image = imageFile; // Set the original image
         });
       }
     } catch (e) {
@@ -155,109 +65,152 @@ class _UploadPhotosState extends State<UploadPhotos> {
     }
   }
 
-  Future<void> _showSavedImagesDialog(BuildContext context) async {
+  void fetchImagesFromFirebaseStorage() async {
     try {
-      final List<String> savedImageUrls = await _getSavedImageUrls();
+      final ListResult result =
+          await FirebaseStorage.instance.ref('images/').list();
 
-      if (savedImageUrls.isNotEmpty) {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('Saved Images'),
-              content: Container(
-                width: double.maxFinite,
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, // Adjust the number of columns as needed
-                  ),
-                  itemCount: savedImageUrls.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final imageUrl = savedImageUrls[index];
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.network(imageUrl),
-                    );
-                  },
-                ),
-              ),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text('Close'),
-                ),
-              ],
-            );
-          },
-        );
-      } else {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('No Saved Images'),
-              content: Text('There are no saved images to display.'),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text('Close'),
-                ),
-              ],
-            );
-          },
-        );
+//
+
+      for (final Reference ref in result.items) {
+        final imageUrl = await ref.getDownloadURL();
+        print('Image URL: $imageUrl'); // Add this line to log the URL
+        setState(() {
+          imageUrls.add(imageUrl);
+        });
       }
     } catch (e) {
-      print('Error fetching saved images: $e');
+      print('Error fetching images: $e'); // Print any errors
     }
   }
 
-  Future<List<String>> _getSavedImageUrls() async {
-    final List<String> savedImageUrls = [];
+  void _showEnlargedImage(String imageUrl) {
+    showDialog(
+      context: context,
+      builder: (_) => Dialog(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pop(context); // Close the dialog on tap
+          },
+          child: CachedNetworkImage(
+            imageUrl: imageUrl,
+            placeholder: (context, url) => CircularProgressIndicator(),
+          ),
+        ),
+      ),
+    );
+  }
 
+  Future<void> _saveImage(String imageUrl) async {
     try {
-      // Replace 'images/' with the actual path to your images in Firebase Storage.
-      final ListResult result =
-          await FirebaseStorage.instance.ref('images/').listAll();
+      final response = await http.get(Uri.parse(imageUrl));
+      if (response.statusCode == 200) {
+        final Uint8List uint8List = response.bodyBytes;
+        final success = await ImageGallerySaver.saveImage(uint8List);
 
-      for (final Reference ref in result.items) {
-        final String downloadUrl = await ref.getDownloadURL();
-        savedImageUrls.add(downloadUrl);
+        if (success != null && success.isNotEmpty) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Image saved to gallery')),
+          );
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Failed to save image')),
+          );
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Failed to download image')),
+        );
       }
     } catch (e) {
-      print('Error fetching image URLs: $e');
+      print(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
     }
-
-    return savedImageUrls;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // ... (rest of your widget code)
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 31, 182, 77),
+        leading: MaterialButton(
+          onPressed: () {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (_) => Home()));
+          },
+          child: Row(
+            children: const [
+              Icon(Icons.arrow_circle_left_outlined,
+                  color: Colors.white, size: 18),
+              Text(
+                " Back",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Color.fromARGB(255, 251, 251, 251),
+                ),
+              ),
+            ],
+          ),
+        ),
+        leadingWidth: 100,
+        title: Image.asset("assets/gemini-icon-transparent.png",
+            color: Colors.white, fit: BoxFit.contain, height: 50),
+        centerTitle: true,
+      ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          // ... (your existing UI elements)
-
-          ElevatedButton(
-            onPressed: _uploadImage,
-            child: Text('Upload Image'),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: _image != null
+                    ? Image.file(_image!)
+                    : Text('No image selected'),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: _uploadImage,
+                child: Text('Upload Image'),
+              ),
+              Center(
+                child: Lottie.network(
+                  'https://assets8.lottiefiles.com/packages/lf20_0zv8teye.json',
+                  height: 200,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Coming Soon...',
+                style: TextStyle(fontSize: 24),
+              ),
+            ],
           ),
-
-          // Button to show the saved images dialog
-          TextButton(
-            onPressed: () {
-              _showSavedImagesDialog(context);
-            },
-            child: Text('Show Saved Images'),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // Number of columns in the grid
+              ),
+              itemCount: imageUrls.length,
+              itemBuilder: (context, index) {
+                final imageUrl = imageUrls[index];
+                return GestureDetector(
+                  onTap: () {
+                    _showEnlargedImage(
+                        imageUrl); // Display enlarged image on tap
+                  },
+                  onLongPress: () {
+                    _saveImage(imageUrl); // Save/download image on long press
+                  },
+                  child: CachedNetworkImage(
+                    imageUrl: imageUrl,
+                    placeholder: (context, url) => CircularProgressIndicator(),
+                  ),
+                );
+              },
+            ),
           ),
-
-          // ... (your remaining UI elements)
         ],
       ),
     );
